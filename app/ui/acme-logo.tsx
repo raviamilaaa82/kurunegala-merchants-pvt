7740b537651d2e8b993f87922f9dc714e036762e
raviamilaaa82 <raviamilaaa82@gmail.com>
2026-04-12 20:54:00 +0530

    Create dashaboard

--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -7,7 +7,9 @@ export default function AcmeLogo() {
       className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
     >
       <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
-      <p className="text-[44px]">Acme</p>
+      <p className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-[44px]">Kurunegala Merchants (Pvt) Ltd</p>
     </div>
   );
 }
+//text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-[44px]
+// original code    text-[44px]
