65ad458aa23daed14e042f87465d601ada96355b
raviamilaaa82 <raviamilaaa82@gmail.com>
2026-05-05 19:05:01 +0530

    test

--- next.config.ts
+++ next.config.ts
@@ -9,7 +9,7 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: "https",
-        hostname: "your-domain.com",
+        hostname: "app.seewalimotors.com",
       },
     ],
     localPatterns: [
